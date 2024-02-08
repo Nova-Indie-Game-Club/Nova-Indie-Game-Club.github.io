f6c2a774781dd749add68b704f14052587b625ad
Koiro <gloridifice@gmail.com>
2024-02-08 12:27:34 +0800

    first commit; finish prototype

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+mod templates;
+
+use perseus::prelude::*;
+
+
+#[perseus::main(perseus_axum::dflt_server)]
+pub fn perseus<G: Html>() -> PerseusApp<G> {
+    PerseusApp::new()
+        .template(templates::index::get_template())
+        .template(templates::about::get_template())
+}
---
+++ src/templates/index.rs
@@ -0,0 +1,44 @@
+use perseus::prelude::*;
+use serde::{Deserialize, Serialize};
+use sycamore::prelude::*;
+
+#[derive(Serialize, Deserialize, ReactiveState, Clone)]
+#[rx(alias = "IndexPageStateRx")]
+struct IndexPageState {
+    greeting: String,
+}
+
+#[auto_scope]
+fn index_page<G: Html>(cx: Scope, state: &IndexPageStateRx) -> View<G> {
+    view! { cx,
+        div(style = "display: flex; flex-direction: column; justify-content: center; align-items: center; height: 95vh;") {
+            h1 { "Welcome to Perseus!" }
+            p {
+                (state.greeting.get())
+                code { "src/templates/index.rs" }
+            }
+        }
+    }
+}
+
+#[engine_only_fn]
+fn head(cx: Scope, _props: IndexPageState) -> View<SsrNode> {
+    view! { cx,
+        title { "Welcome to Perseus!" }
+    }
+}
+
+#[engine_only_fn]
+async fn get_build_state(_info: StateGeneratorInfo<()>) -> IndexPageState{
+    IndexPageState{
+        greeting: "Hello World!".to_string(),
+    }
+}
+
+pub fn get_template<G: Html>() -> Template<G> {
+    Template::build("index")
+        .build_state_fn(get_build_state)
+        .view_with_state(index_page)
+        .head_with_state(head)
+        .build()
+}
