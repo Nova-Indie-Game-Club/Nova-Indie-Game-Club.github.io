8427b4cf325337178b01de96750c8945587df467
Koiro <1165997495@qq.com>
2024-03-15 17:42:42 +0800

    refactor: remove SvgFile component

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,24 +1,8 @@
-use sycamore::{builder, prelude::*};
+use sycamore::prelude::*;
 
-use crate::tool::statics;
 
 pub mod work;
 pub mod footer;
-
-
-#[derive(Prop)]
-pub struct SvgFileProps{
-    path: &'static str,
-    #[builder(default)]
-    class: &'static str
-}
-#[component]
-pub fn SvgFile<G: Html>(cx: Scope, prop: SvgFileProps) -> View<G>{
-    view!{ cx, 
-        object(data=(statics(prop.path)), class=(prop.class))
-    }
-}
-
 
 #[derive(Prop)]
 pub struct SvgCodeProps{
