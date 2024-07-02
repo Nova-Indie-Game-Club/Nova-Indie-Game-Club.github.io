60eb5bfdda15991caf57846aa3e893b4d181ef9d
Koiro <1165997495@qq.com>
2024-07-02 12:24:10 +0800

    feat: github page

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,5 @@ pub mod tool;
 
 pub mod components;
 pub mod templates;
+
+pub mod error_views;
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use nova_website::templates;
+use nova_website::error_views;
 use perseus::prelude::*;
 
-#[perseus::main(perseus_axum::dflt_server)]
+#[perseus::main_export]
 pub fn perseus<G: Html>() -> PerseusApp<G> {
     PerseusApp::new()
         .template(templates::index::get_template())
         .template(templates::about::get_template())
+        .error_views(error_views::get_error_views())
 }
